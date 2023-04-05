5e0ef259d4f4219710da43f16cabcf626a153d1a
reueck <65670634+reueck@users.noreply.github.com>
2023-04-05 22:58:00 +0200

    Added cover image to cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,9 +10,13 @@ const carsList = ref(cars)
 		<h1>Cars in stock:</h1>
 		<div class="container">
 			<router-link :to="`/cars/${car.id}`" v-for="car in carsList" :key="car.id" class="car-card">
-           
-				<h2>{{ car.year }} {{ car.name }} {{ car.model }}</h2>
+				<div class="car-card-cover">
+					<img :src="car.image" alt="car image" />
+				</div>
+				<div class="car-card-body">
+					<h2>{{ car.year }} {{ car.name }} {{ car.model }}</h2>
 				<p>Price: {{ car.price }} $</p>
+				</div>
 			
             </router-link>
 		</div>
@@ -32,20 +36,35 @@ h1 {
 }
 
 .car-card {
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows:2fr 1fr;
 	width: 300px;
 	height: 300px;
+	margin: 1rem;
 	background-color: #333;
 	color: #fff;
-	padding: 1rem;
-	margin: 1rem;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-evenly;
-	align-items: center;
+	overflow: hidden;
     transition: all 0.3s ease-in-out;
 }
 
 .car-card:hover {
-    box-shadow: 0 5px 5px #00000082;
+	box-shadow: 0 5px 5px #00000082;
+}
+
+.car-card-cover {
+	width: 300px;
+	grid-area: 1 / 1 / 2 / 1;
+}
+.car-card-cover img{
+	width: 100%;
+	height: 100%;
+	aspect-ratio: 16/9;
+	object-fit: cover;
+	background-blend-mode: darken;
+}
+.car-card-body {
+	grid-area: 2 / 1 / 3 / 1;
+	padding: 1rem;
 }
 </style>
